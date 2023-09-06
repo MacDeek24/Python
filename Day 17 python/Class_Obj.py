85afb92150df1bbc369198cc4a79c2be54505481
Deek Macwan <137483479+MacDeek24@users.noreply.github.com>
2023-09-06 17:17:37 +0530

    Add files via upload

---
+++ Day 17 python/Class_Obj.py
@@ -0,0 +1,13 @@
+#introduction of class and Object , and How to create it
+class User(): # class create
+    # def __init__(self,user_id, user_name):
+    #     self.id = user_id
+    #     self.name = user_name
+    pass
+    
+
+user_1 = User() # object create
+user_1.id = "007"
+user_1.name = "Deek"
+print(user_1.id)
+print(user_1.name)
